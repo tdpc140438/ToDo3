91601819b5f58662b41146ecfe074d9927aec6c9
JC-21\140438 <te140438@gmail.com>
2018-01-15 15:04:51 +0900

    成功画面を完成 履歴画面の作成

--- app/src/main/java/com/example/a140438/todo3/HistoryActivity.java
+++ app/src/main/java/com/example/a140438/todo3/HistoryActivity.java
@@ -1,5 +1,7 @@
 package com.example.a140438.todo3;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -19,7 +21,21 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        //SQLite
+        OpenHelper helper = new OpenHelper(this);
+        final SQLiteDatabase db = helper.getWritableDatabase();
+
         //forでpastテーブルの件数ぶん取得し、1件ごとにチェックボタンと2つのテキストビューを横並びにしたレイアウトを history_length_layout に追加する
+
+        Cursor c_past = null;
+        //select
+        String sql_past = "SELECT * FROM past;";
+        c_past = db.rawQuery(sql_past, new String[]{});
+        boolean mov_past = c_past.moveToFirst();
+        /*for(int i = 0;){
+        }*/
+
 
 
 
